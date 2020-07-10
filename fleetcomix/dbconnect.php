97c7e19291856c3b2d385792a0f07ec6624d7867
adediperhema <adediperhema@gmail.com>
2020-07-10 23:58:18 +0100

    Add files via upload

--- fleetcomix/dbconnect.php
+++ fleetcomix/dbconnect.php
@@ -1,8 +1,8 @@
 <?php 
-define('DB_HOST', 'localhost'); 
-define('DB_NAME', 'book_store'); 
-define('DB_USER','root'); 
-define('DB_PASSWORD','user123'); 
+define('DB_HOST', 'us-cdbr-east-02.cleardb.com'); 
+define('DB_NAME', 'heroku_e3b02fd7a572122'); 
+define('DB_USER','bd5d675724d4b5'); 
+define('DB_PASSWORD','85ef41db'); 
 
 $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 $db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysql_error()); 
